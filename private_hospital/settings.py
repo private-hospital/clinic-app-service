5ae92c6499c0e9dcc85a3d1efe473333f8617989
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-03 16:44:31 +0200

    Add ELB to origins

--- private_hospital/settings.py
+++ private_hospital/settings.py
@@ -27,14 +27,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG", default=0))
 
-ALLOWED_HOSTS=['*']
-CSRF_TRUSTED_ORIGINS=['*']
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
-# CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
-# print(f"CSRF_ORIGINS - {CSRF_TRUSTED_ORIGINS}")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
+print(f"ALLOWED_HOSTS - {ALLOWED_HOSTS}")
+print(f"CSRF_ORIGINS - {CSRF_TRUSTED_ORIGINS}")
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
