2cec9337ecb50e831919a30bb8ca4955575599f8
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-03 20:00:22 +0200

    Fix port

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo "Starting Django application..."
 python3 manage.py migrate --noinput
-python3 -m gunicorn --bind 0.0.0.0:8000 --workers 3 private_hospital.wsgi:application
+python3 -m gunicorn --bind 0.0.0.0:80 --workers 3 private_hospital.wsgi:application
