08d8f064860955f5888f543fb3c8c29ed87f010d
Maksym Panov <work.maksym.panov@gmail.com>
2025-03-03 18:35:59 +0200

    Trying to create nginx container

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,4 +2,4 @@
 echo "Starting Django application..."
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
-python3 -m gunicorn --bind 0.0.0.0:80 --workers 3 private_hospital.wsgi:application
+python3 -m gunicorn --bind 0.0.0.0:8000 --workers 3 private_hospital.wsgi:application
