0e27d7943c51ab92f928f40f4fe4fc7ede4a4e6d
Valeriia Nedashkivska <valeriia.nedashkivska@hneu.net>
2025-03-03 12:11:47 +0200

    Update settings

--- private_hospital/settings.py
+++ private_hospital/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ri0ox+x!bnxr@p%+s#h#7@e_(-9(qb$dh#$-go3!c1-m%l%3!!'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
